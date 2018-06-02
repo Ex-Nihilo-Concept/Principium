586ebc420d3a574292f30f0434b00baa2a51781d
root <root@server1.miningcache.website>
2018-06-02 21:02:42 +0000

    adding files

---
+++ README.txt
@@ -0,0 +1,11 @@
+Ex Nihilo
+Latin for: from nothing comes something.
+Welcome to the Ex Nihilo community!
+Everything, Everywhere is created through this concept.
+The Big Bang is the best example of Ex Nihilo. The moment of creation. There was nothing, then.. there was something. Art, Music, Philosophy, Religion, Science  and Life itself are all Ex Nihilo.
+Our coin is based on this concept of making something from nothing. Value, Trust, Faith and Mercy are the 4 corner stones upon which Ex Nihilo will become an asset not only as currency unit of worth but also an asset in every day life.
+
+Usage: Download the source code or prebuilt tar.gz
+Linux CLI wallet only! Windows wallet, GUI and web wallet coming shortly
+Thank you for your patience :)
+
---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+//
+// This file is part of Bytecoin.
+//
+// Bytecoin is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Bytecoin is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Bytecoin.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid BYTECOIN_NETWORK = { {  0x60, 0x9a, 0x22, 0xb8, 0x30, 0x9d, 0x1b, 0xbf, 0x8f, 0xe3, 0x34, 0x3b, 0x2b, 0x31, 0xcd, 0x01  } };
+
+}
+
